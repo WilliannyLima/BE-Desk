e4dac2696c7df48ba34f6e252068e895ba153029
igor murilo <igormurilo.ac.21@gmal.com>
2025-10-09 14:40:42 -0300

    .

--- bedesk/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def inicio(request):
-    return render(request, 'inicio.html')
--- blocoe/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.inicio, name="inicio"),
-    path("recursos/", views.recursos, name="ver_recursos"),
-    path('login/', views.login, name="login")
-]
